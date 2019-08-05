4d032e3608ed16511c6731ddd7c2abfe4ad752fc
rmmahajan <manvi.mahajan20@gmail.com>
2019-08-05 19:28:11 +0530

    more changes

--- app/src/main/java/com/example/appw/MainActivity.java
+++ app/src/main/java/com/example/appw/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.appw;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import androidx.appcompat.widget.Toolbar;
 import androidx.viewpager.widget.ViewPager;
@@ -24,6 +31,7 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseUser currentUser;
     private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +41,7 @@ public class MainActivity extends AppCompatActivity {
 
         mAuth = FirebaseAuth.getInstance();
         currentUser = mAuth.getCurrentUser();
+        RootRef = FirebaseDatabase.getInstance().getReference();
 
         mtoolbar = findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mtoolbar);
@@ -56,12 +65,46 @@ public class MainActivity extends AppCompatActivity {
         {
             sendUserToLoginActivity();
         }
+        else
+        {
+            VerifyUserExistance();
+        }
+
+    }
+
+    private void VerifyUserExistance() {
+
+        String currentUserId = mAuth.getCurrentUser().getUid();
+
+        RootRef.child("Users").child(currentUserId).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot)
+            {
+                if((dataSnapshot.child("name").exists()))
+                {
+                    Toast.makeText(MainActivity.this,"Welcome",Toast.LENGTH_LONG).show();
+                }
+                else
+                {
+                    sendUserToSettingsActivity();
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError)
+            {
+
+
+            }
+        });
 
     }
 
     private void sendUserToLoginActivity() {
 
         Intent loginIntent = new Intent(MainActivity.this,LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(loginIntent);
         finish();
     }
@@ -69,7 +112,9 @@ public class MainActivity extends AppCompatActivity {
     private void sendUserToSettingsActivity() {
 
         Intent settingIntent = new Intent(MainActivity.this,SettingsActivity.class);
+        settingIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
         startActivity(settingIntent);
+        finish();
     }
 
 
--- app/src/main/java/com/example/appw/SettingsActivity.java
+++ app/src/main/java/com/example/appw/SettingsActivity.java
@@ -1,11 +1,24 @@
 package com.example.appw;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import de.hdodenhof.circleimageview.CircleImageView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -13,13 +26,79 @@ public class SettingsActivity extends AppCompatActivity {
     private Button UpdateAccountSettings;
     private EditText userName,userStatus;
     private CircleImageView userProfileImage;
+    private String currentUserID;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
+        mAuth = FirebaseAuth.getInstance();
+        currentUserID = mAuth.getCurrentUser().getUid();
+        RootRef = FirebaseDatabase.getInstance().getReference();
+
         InitializeFields();
+
+        UpdateAccountSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                UpdateSettings();
+
+            }
+        });
+    }
+
+    private void UpdateSettings() {
+
+        String setUserName = userName.getText().toString();
+        String setStatus = userStatus.getText().toString();
+
+        if(TextUtils.isEmpty(setUserName))
+        {
+            Toast.makeText(SettingsActivity.this,"Please write your username",Toast.LENGTH_SHORT).show();
+        }
+        else if(TextUtils.isEmpty(setStatus))
+        {
+            Toast.makeText(SettingsActivity.this,"Please write your status",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            HashMap<String,String> profileMap = new HashMap<>();
+
+            profileMap.put("uid",currentUserID);
+            profileMap.put("name",setUserName);
+            profileMap.put("status",setStatus);
+
+            RootRef.child("Users").child(currentUserID).setValue(profileMap)
+                    .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+
+                            if(task.isSuccessful())
+                            {
+                                sendUserToMainActivity();
+                                Toast.makeText(SettingsActivity.this,"Updated Successfully",Toast.LENGTH_LONG).show();
+                            }
+                            else
+                            {
+                                Toast.makeText(SettingsActivity.this,"Error...",Toast.LENGTH_LONG).show();
+                            }
+
+                        }
+                    });
+        }
+
+    }
+
+    private void sendUserToMainActivity() {
+
+        Intent mainIntent = new Intent(SettingsActivity.this,MainActivity.class);
+        mainIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(mainIntent);
+        finish();
     }
 
     private void InitializeFields() {
